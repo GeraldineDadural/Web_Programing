0934d82d8c57eb776ebdd24907cf031773665a55
GeraldineDadural <133655410+GeraldineDadural@users.noreply.github.com>
2023-05-16 09:10:39 +0800

    Add files via upload

---
+++ components/index.js
@@ -0,0 +1,134 @@
+function show(){
+    document.getElementById("ass").style.display = "block";
+    document.getElementById("acts").style.display = "none";
+    document.getElementById("pro").style.display = "none";
+    document.getElementById("tb1").style.display = "none";
+    document.getElementById("ss1").style.display = "none";
+    document.getElementById("more").style.display = "none";
+}
+function hide(){
+    document.getElementById("ass").style.display = "none";
+}
+function show2(){
+    document.getElementById("acts").style.display = "block";
+    document.getElementById("ass").style.display = "none";
+    document.getElementById("pro").style.display = "none";
+    document.getElementById("tb1").style.display = "none";
+    document.getElementById("ss1").style.display = "none"; 
+    document.getElementById("more").style.display = "none";
+}
+function hide2(){
+    document.getElementById("acts").style.display = "none";
+}
+function show3(){
+    document.getElementById("pro").style.display = "block";
+    document.getElementById("ass").style.display = "none";
+    document.getElementById("acts").style.display = "none"; 
+    document.getElementById("tb1").style.display = "none";
+    document.getElementById("ss1").style.display = "none";
+    document.getElementById("more").style.display = "none";
+}
+function hide3(){
+    document.getElementById("pro").style.display = "none";
+}
+
+function show4(){
+    document.getElementById("more").style.display = "block";
+    document.getElementById("ass").style.display = "none";
+    document.getElementById("pro").style.display = "none";
+    document.getElementById("acts").style.display = "none"; 
+    document.getElementById("tb1").style.display = "none";
+    document.getElementById("ss1").style.display = "none";
+}
+function hide4(){
+    document.getElementById("more").style.display = "none";
+}
+
+function show11(){
+    document.getElementById("tb1").style.display = "block";
+    document.getElementById("ass").style.display = "none";
+    document.getElementById("acts").style.display = "none";
+    document.getElementById("pro").style.display = "none";
+    document.getElementById("ss1").style.display = "none";  
+}
+
+function hide11(){
+    document.getElementById("tb1").style.display = "none";
+    document.getElementById("ass").style.display = "block";
+    document.getElementById("ss1").style.display = "none";
+}
+
+function show12(){
+    document.getElementById("ss1").style.display = "block";
+    document.getElementById("ass").style.display = "none";
+    document.getElementById("acts").style.display = "none";
+    document.getElementById("pro").style.display = "none";  
+}
+function hide12(){
+    document.getElementById("ss1").style.display = "none";
+    document.getElementById("ass").style.display = "block";
+    document.getElementById("tb1").style.display = "none";
+}
+function show13(){
+    document.getElementById("fb1").style.display = "block";
+    document.getElementById("ass").style.display = "none";
+    document.getElementById("acts").style.display = "none";
+    document.getElementById("pro").style.display = "none";  
+}
+function hide13(){
+    document.getElementById("fb1").style.display = "none";
+    document.getElementById("ass").style.display = "block";
+    document.getElementById("tb1").style.display = "none";
+}
+
+function show14(){
+    document.getElementById("ccs1").style.display = "block";
+    document.getElementById("ass").style.display = "none";
+    document.getElementById("acts").style.display = "none";
+    document.getElementById("pro").style.display = "none";  
+}
+function hide14(){
+    document.getElementById("ccs1").style.display = "none";
+    document.getElementById("ass").style.display = "block";
+    document.getElementById("tb1").style.display = "none";
+    document.getElementById("pro1").style.display = "none";
+}
+
+
+function show15(){
+    document.getElementById("move1").style.display = "block";
+    document.getElementById("ccs1").style.display = "none";
+    document.getElementById("ass").style.display = "none";
+    document.getElementById("acts").style.display = "none";
+    document.getElementById("pro").style.display = "none";  
+}
+function hide15(){
+    document.getElementById("move1").style.display = "none";
+    document.getElementById("ccs1").style.display = "none";
+    document.getElementById("ass").style.display = "block";
+    document.getElementById("tb1").style.display = "none";
+    document.getElementById("pro1").style.display = "none";
+}
+
+
+function show16(){
+    document.getElementById("loop1").style.display = "block";
+    document.getElementById("ccs1").style.display = "none";
+    document.getElementById("ass").style.display = "none";
+    document.getElementById("acts").style.display = "none";
+    document.getElementById("pro").style.display = "none";  
+    document.getElementById("move1").style.display = "none";
+}
+function hide16(){
+    document.getElementById("loop1").style.display = "none";
+    document.getElementById("ccs1").style.display = "none";
+    document.getElementById("ass").style.display = "block";
+    document.getElementById("tb1").style.display = "none";
+    document.getElementById("pro1").style.display = "none";
+    document.getElementById("move1").style.display = "none";
+}
+
+
+function show21(){
+    document.getElementById("ass").style.display = "block";
+}
